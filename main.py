fd6fd370e34083445c7adda178852cb398f1a0ed
叙白 <61175126+xubai2001@users.noreply.github.com>
2025-01-20 15:07:38 +0800

    fix

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import sys,os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__)) # get the folder that your file is in
 sys.path.append(parent_folder_path) # add the folder to path
 sys.path.append(os.path.join(parent_folder_path, "lib")) # add a 'lib' folder that is in the same dir as your file to path
+sys.path.append(os.path.join(parent_folder_path, "plugin"))
 sys.path.append(os.path.join(parent_folder_path, "venv", "lib", "site-packages")) # add your venv to path
 
 
-from flogin import Plugin, Query
+from flogin import Plugin, Query, Result
 
 plugin = Plugin()
 
 @plugin.search()
-async def compare_results(data: Query):
-    result = await plugin.api.fuzzy_search(data.text, "Flow")
-    return f"Flow: {result.score}",
+async def on_query(data: Query):
+    yield f"Your text is: {data.text}"
+    yield f"Your keyword is: {data.keyword}"
+    yield Result(f"Your raw text is: {data.raw_text}", sub="keyword + text")
 
-plugin.run()
+
+if __name__ == "__main__":
+    plugin.run()
