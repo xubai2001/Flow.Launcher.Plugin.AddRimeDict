6626a71a356b33bd0d86391edcd05c0c3e649309
叙白 <61175126+xubai2001@users.noreply.github.com>
2025-01-20 13:21:21 +0800

    fix

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import sys,os
-parent_folder_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(parent_folder_path)
-sys.path.append(os.path.join(parent_folder_path, 'lib'))
-sys.path.append(os.path.join(parent_folder_path, 'plugin'))
+parent_folder_path = os.path.abspath(os.path.dirname(__file__)) # get the folder that your file is in
+sys.path.append(parent_folder_path) # add the folder to path
+sys.path.append(os.path.join(parent_folder_path, "lib")) # add a 'lib' folder that is in the same dir as your file to path
+sys.path.append(os.path.join(parent_folder_path, "venv", "lib", "site-packages")) # add your venv to path
 
 
-# from plugin.main import HelloWorld
-
-# if __name__ == "__main__":
-#     HelloWorld()
 from flogin import Plugin, Query
+
 plugin = Plugin()
+
 @plugin.search()
 async def compare_results(data: Query):
     result = await plugin.api.fuzzy_search(data.text, "Flow")
-    return f"Flow: {result.score}"
+    return f"Flow: {result.score}",
+
+plugin.run()
